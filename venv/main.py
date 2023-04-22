616f3c432db70ab3b53f702073b7fea43296a0f4
Svetlikova <svetlikovahana@proton.me>
2023-04-22 20:25:46 +0200

    refactoring, sample 1000 rolls in 100 runs

--- venv/main.py
+++ venv/main.py
@@ -37,14 +37,14 @@ categories = [3, 4, 5, 6]
 # Probability measure - assign probability based on pity - value changed by previous state
 def probability_set(pity):
     # Increase the probability of RaritySix
-    newProbability = [(((1 - pity) / 0.98) * 0.4), (((1 - pity) / 0.98) * 0.5), (((1 - pity) / 0.98) * 0.08), pity]
+    new_probability = [(((1 - pity) / 0.98) * 0.4), (((1 - pity) / 0.98) * 0.5), (((1 - pity) / 0.98) * 0.08), pity]
     # Decrese the other probabilities
-    probabilities = newProbability
+    probabilities = new_probability
     # Normalization of the probabilities - to ensure their sum equals to 1
-    totalProbability = round(sum(newProbability), 16)
-    if totalProbability != 1:
-        print(totalProbability)
-        return ("Total sum of probabilities has to equal!")
+    total_probability = round(sum(new_probability), 16)
+    if total_probability != 1:
+        print(total_probability)
+        raise "Total sum of probabilities has to equal! instead its value is" + str(total_probability) +"!"
     return probabilities
 
 probabilities = probability_set(pity)
@@ -52,15 +52,15 @@ probabilities = probability_set(pity)
 
 
 # Functions selecting specific operator in case defined rarity has been selected
-def selectOperatorR3():
+def select_operator_r3():
     return ''.join(random.choices(rarity_three))
 
 
-def selectOperatorR4():
+def select_operator_r4():
     return ' '.join(random.choices(rarity_four))
 
 
-def selectOperatorR5():
+def select_operator_r5():
     # Specific operators rate up - 50% chance one of the 5 star operators will be one of selected
     rate_up = ['Franka', 'Proviso']
     rate_up_choice = ['rate_up', 'no_rate_up']
@@ -74,7 +74,7 @@ def selectOperatorR5():
     return result
 
 
-def selectOperatorR6():
+def select_operator_r6():
     rate_up = ['Mlynar']
     rate_up_choice = ['rate_up', 'no_rate_up']
     chose = ''.join(random.choices(rate_up_choice))
@@ -87,27 +87,22 @@ def selectOperatorR6():
     return result
 
 
-selected = None
-'''function selects operation depending on selected rarity'''
-
-
-
 
 # Based on rarity this function selects operator
-def select_operator(selectRarity):
+def select_operator(select_rarity):
     selected = None
-    if selectRarity == 4:
+    if select_rarity == 4:
         selected = selectOperatorR4()
-    elif selectRarity == 3:
+    elif select_rarity == 3:
         selected = selectOperatorR3()
-    elif selectRarity == 5:
+    elif select_rarity == 5:
         selected = selectOperatorR5()
-    elif selectRarity == 6:
+    elif select_rarity == 6:
         selected = selectOperatorR6()
     return selected
 
 
-# CSV writer, function used anywhere where is needed to transfer data into csv
+# CSV writer, function used to transfer data into csv
 # Change the name of CSV file for more outputs
 def csv_write_data(data):
     with open('simulation.csv', 'a', newline='') as csvfile:
@@ -116,6 +111,10 @@ def csv_write_data(data):
         print("_ _ _ _ _ _ _ _ _ _ _ _ ")
         print(data)
 
+def csv_write_cost(rows_cost):
+    with open('cost.csv', 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(rows_cost)
 
 # The main function of simulation
 def simulation():
@@ -123,10 +122,10 @@ def simulation():
     number_of_rolls = 1000
     stop = False
     roll = 0
-    pity = 0.00
+    pity = 0.02
     change = False
     pity_counter = 0
-    cost = - 600
+    cost = 0
     while not stop:
         probabilities = probability_set(pity)
         roll += 1
@@ -140,7 +139,7 @@ def simulation():
             data = [roll, rarity, operator]
             csv_write_data(data)
             if rarity != 6:
-                pity_counter += 1
+                pity_counter += 0.02
             else:
                 pity_counter = 0
                 pity = round (0.02, 2)
@@ -155,29 +154,37 @@ def simulation():
             if rarity != 6 and pity_counter < 50:
                 pity_counter += 1
                 if pity_counter == 50:
-                    pity += 1
+                    pity += 0.02
             elif rarity != 6 and pity_counter > 50:
                 pity += 1
                 pity = round(pity, 2)
             elif rarity == 6:
                 change = True
-                pity = 0.00
+                pity = 0.02
                 pity_counter = 0
                 # In case we want to predict real situation we set the 'stop' condition here
                 # print('The desired outcome was achieved, rational player stops rolling.')
                 if operator == 'Mlynar':
                     stop = True
+                    rows_cost = [roll, cost, 1]
+                    print('Desired outcome achieved, simulation finished. Total number of rolls was '+str(number_of_rolls)
+                  + ' and total cost was '+ str(cost) + '.')
+                    csv_write_cost(rows_cost)
         elif roll >= number_of_rolls:
+            cost -= 600
             stop = True
+            rows_cost = [roll, cost, 0]
+            csv_write_cost(rows_cost)
             print('simulation finished, total number of rolls was '+str(number_of_rolls)
                   + ' and total cost was '+ str(cost) + '.')
 
 
 # Run simulation 'n' number of times
-n = 1
+# For analysis only set 'n' to '0'
+n = 0
 for _ in range(n):
     simulation()
-    print('Simulation finished ' + str(n) + ' time(s)')
+    print('Simulation finished ' + str(n) + ' time(s).')
 
 
 # Visualization of the outputs of simulation
@@ -186,10 +193,21 @@ print(df.describe())
 # Histogram
 plt.hist(df['Rarity'], bins=4)
 plt.title('Histogram of rarity distribution \n')
+plt.xlabel('Rarity')
+plt.ylabel('Number of rolls')
 plt.show()
 # Boxplot
-df.boxplot('Roll Number', by='Rarity')
+df.boxplot('Roll number', by='Rarity')
 plt.title('\n')
 plt.xlabel('Rarity')
 plt.ylabel('Roll Number')
 plt.show()
+
+# Data about results, cost prediction
+df = pd.read_csv('cost.csv')
+print(df.describe())
+# Histogram
+plt.hist(df['Roll number'], bins=1)
+plt.title('Histogram Result  \n')
+plt.show()
+# Boxplot
